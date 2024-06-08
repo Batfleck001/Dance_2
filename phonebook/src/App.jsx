6fe9052700fa1a58812daadfdefb85aded9a00cf
Batfleck001 <harishsekar1423@gmail.com>
2024-06-08 18:12:06 +0530

    Done Exercise 2.9 filtering

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const[persons,setpersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      id : 1
-    }
+  const [persons, setpersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-
+  const [query, setquery] = useState('')
   const [newName, setnewName] = useState('')
+  const [newNumber, setnewNumber] = useState(0)
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -21,32 +22,47 @@ const App = () => {
     else{
       const newpersonObject = {
       name : newName,
+      number: newNumber,
       id: persons.length+1
     }
     setpersons(persons.concat(newpersonObject))
   }
   setnewName('')
+  setnewNumber(0)
   }
-    const handlechange = (event) =>{
+    const handlenamechange = (event) =>{
       event.preventDefault()
       setnewName(event.target.value)
     }
+    const handlenumberchange = (event) =>{
+      event.preventDefault()
+      setnewNumber(event.target.value)
+    }
+    const handlequery = (event) =>{
+      event.preventDefault()
+      setquery(event.target.value)
+    }
+    
+    const filtered = query != '' ? persons.filter(val => val.name.toLowerCase().includes(query.toLowerCase())) : []
 
-
-
+    
   return (
     <>
       <h2>Phonebook</h2>
+      Filter shown with : <input onChange={handlequery}/>
+      {filtered.map(val => <p>{val.name} : {val.number}</p>)}
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name : <input onChange={handlechange}/>
+          name : <input onChange={handlenamechange}/> <br/>
+          number : <input onChange={handlenumberchange}/>
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Number</h2>
-      {persons.map(val => <p key={val.id}>{val.name}</p>)}
+      {persons.map(val => (<p key={val.id}>{val.name} : {val.number}</p>))}
     </>
   )
 }
